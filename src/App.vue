1ed56c22e9c0968c466a5537b2d6131dfaa88cc0
ColluraV <valerio.18.02.89@hotmail.it>
2023-10-16 17:59:54 +0200

    inizializzato progetto+funzione recupero dati api

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+import axios from 'axios';
+
+
+export default{
+
+  data(){
+    return{
+      projects:[],
+      pagination:{}
+    };
+  },
+  methods:{
+    
+    dataCaller(){
+      axios.get("http://127.0.0.1:8000/api/projects")
+      .then((response)=>{
+          
+        console.log(response);
+        this.projects=response.data.data;
+
+       })
+    }
+
+  },
+  mounted(){
+    this.dataCaller();
+  }
+}
+
+</script>
+
+<template>
+
+  <h1>dfsds</h1>
+
+</template>
+
+<style scoped>
+/*.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}*/
+</style>
