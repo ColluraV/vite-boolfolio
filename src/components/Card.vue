f9042423a0a4224bb7098c242b8b37d64e6c6c99
ColluraV <valerio.18.02.89@hotmail.it>
2023-10-16 22:35:14 +0200

    completato + hover su pagina attiva

---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    props: {
+        title: String,
+        description: Text,
+        type: String,
+        techs: Array,
+        image: Text,
+        link: Text,
+    }
+};
+
+
+</script>
+
+
+<template>
+    <div class="card p-4 m-2">
+        <img :src="image" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <p class="card-text">{{ description }}</p>
+        </div>
+        <div class="d-flex">
+            <div class="badge rounded-pill text-bg-info" v-for="tech in techs">
+                {{ tech.name }}
+            </div>
+        </div>
+        <div class="card-body">
+            <a :href="link" class="card-link">{{ link }}</a>
+        </div>
+    </div>
+</template>
+
+
+<style>
+@import "../style.css";
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+};
+
+
+</script>
+
+
+<template>
+
+
+<nav class="d-flex justify-content-between">
+
+    <a href="http://127.0.0.1:8000/">Home</a>
+
+    <a href="http://127.0.0.1:8000/login">Login</a>
+
+
+</nav>
+
+
+</template>
+
+
+<style>
+@import "../style.css";
+</style>
