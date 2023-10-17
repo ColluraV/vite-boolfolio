7ecef5dae372fba75ec406b5ab9410a0ee3ac715
ColluraV <valerio.18.02.89@hotmail.it>
2023-10-17 14:29:14 +0200

    impostato il router

--- src/router.js
+++ src/router.js
@@ -1,2 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
-import 
+
+//importazione componenti vue
+import Home from "./pages/Home.vue";
+import Show from "./pages/Show.vue";
+
+
+//definizione rotte
+const routes=[
+    {//rotta alla homepage
+        path:"/",
+        name: "home",
+        component: Home,
+    },
+    {//rotta allo show
+        path:"/project/:id",
+        name:"project.show",
+        component: Show,
+    }
+]
+
+//istanza di router
+const router=createRouter({
+    //gestione dell'url al cambio pagina
+    history: createWebHistory(),
+    //restituiamo l'array delle rotte creato
+    routes
+});
+
+export {router};
